96faf1479744e10f4e1b995aaddaf7e0691ec13d
MariaEDuarda005 <mariaesferreira05@gmail.com>
2024-12-09 16:49:13 -0300

    projeto vue

---
+++ aula5 vue/vue-project/src/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import MyComponent from './components/MyComponent.vue'
+
+/** Hooks
+ * defineProps
+ * -v-bind
+ * -v-if
+ * -v-else
+ * -v-else-if
+ * -v-for
+ */
+
+const model = defineModel();
+const texts = ref(["My text", "hello world", model]);
+
+const list = ref([
+  {
+    id: 1,
+    text: model
+  },
+  {
+    id: 2,
+    text: "Hello world"
+  }
+])
+
+const sum = ref(0)
+
+
+</script>
+
+<template>
+  {{ sum }}
+
+  <div v-for="value in list" v-bind:key="value.id">
+    {{ value.text }}
+  </div>
+
+  <MyComponent
+    v-model="texts[0]"
+    @onSend="sum += $event"
+  />
+
+  <input v-model="model">
+
+</template>
+
+<style scoped>
+</style>
---
+++ aula5 vue/vue-project/src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+defineProps([ 'onSend' ])
+const count = ref(0)
+const model = defineModel();
+
+</script>
+
+<!-- <marquee></marquee> - faz andar -->
+
+<template>
+  <input v-model="model">
+  <button @click="$emit(onSend, ++count)">Send Data!</button>
+</template>
+
+<style scoped>
+</style>
